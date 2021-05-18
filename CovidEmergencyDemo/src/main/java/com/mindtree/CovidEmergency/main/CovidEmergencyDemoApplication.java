757a5fbfab8f07d3a7474e3b5b306e20f0d3161e
M1064208 <M1064208@MT-PG02AGW6>
2021-05-18 13:01:59 +0530

    commit all files

---
+++ CovidEmergencyDemo/src/main/java/com/mindtree/CovidEmergency/main/CovidEmergencyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mindtree.CovidEmergency.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidEmergencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidEmergencyDemoApplication.class, args);
+	}
+
+}
---
+++ CovidEmergencyDemo/src/test/java/com/mindtree/CovidEmergency/main/CovidEmergencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.CovidEmergency.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidEmergencyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
